d968b26c3abfbb34a417978d2227e9555eb023c4
Hui137 <junxionghui.137@gmail.com>
2025-02-22 11:22:56 -0500

    User input

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
+from flask import Flask, render_template, request, redirect, url_for
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+@app.route('/test', methods = ["GET", "POST"])
+def test():
+    if request.method == "POST":
+       path = request.form.get("path")
+       return "You Entered: " + path
+    return render_template('test.html', title='File Classifier')
