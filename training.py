dcf8ae328f322b3a3fe191aad1acab9cf7e44efa
CoffeCup12 <shaozhu19@gmail.com>
2025-02-22 12:04:54 -0500

    pdf reader

---
+++ training.py
@@ -0,0 +1,9 @@
+from pypdf import PdfReader
+
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with open(pdf_path, 'rb') as file:
+        reader = PdfReader(file)
+        for page in reader.pages:
+            text += page.extract_text() or ""
+    return text
