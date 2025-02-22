38df43eae9f8b54ae4699038441b09ba6c2af0fe
CoffeCup12 <shaozhu19@gmail.com>
2025-02-22 10:35:46 -0500

    separate classes

--- model.py
+++ model.py
@@ -3,22 +3,64 @@ import torch.nn as nn
 import torch.nn.functional as F 
 import torch.optim as optim
 
+
+class WordLevelNetwork(nn.Module):
+    def __init__(self, inputSize, hiddenSize, numLayers, vocabSize):
+        super(WordLevelNetwork, self).__init__()
+        self.embedding = nn.Embedding(vocabSize, inputSize)
+        self.gru = nn.GRU(inputSize, hiddenSize, numLayers, batch_first=True, bidirectional=True)
+        self.attention = nn.Sequential(
+            nn.Linear(hiddenSize*2, 1),
+            nn.Tanh()
+        )
+    
+    def forward(self, inputText):
+        embedded = self.embedding(inputText)
+        output, _ = self.gru(embedded)
+        attentionWeights = F.softmax(self.attention(output), dim=1)
+        contextVector = torch.sum(attentionWeights * output, dim=1)
+        return contextVector
+    
+class SentenceLevelNetwork(nn.Module):
+    def __init__(self, inputSize, hiddenSize, numLayers):
+        super(SentenceLevelNetwork, self).__init__()
+        self.gru = nn.GRU(inputSize, hiddenSize, numLayers, batch_first=True, bidirectional=True)
+        self.attention = nn.Sequential(
+            nn.Linear(hiddenSize*2, 1),
+            nn.Tanh()
+        )
+    
+    def forward(self, inputText):
+        output, _ = self.gru(inputText)
+        attentionWeights = F.softmax(self.attention(output), dim=1)
+        contextVector = torch.sum(attentionWeights * output, dim=1)
+        return contextVector
+
 class HANModel(nn.Module):
     
-    def __init__(self, wordLevelVariables, sentenceLevelVariables):
+    def __init__(self, wordLevel, sentenceLevel, numCategories):
         super(HANModel, self).__init__()
-        self.embedding = nn.Embedding(wordLevelVariables['vocabSize'], wordLevelVariables['input'])
-        self.wordEncoder = nn.GRU(wordLevelVariables['inputSize'], wordLevelVariables['hiddenSize'], wordLevelVariables['numLayers'], batch_first=True, bidirectional=True)  
-        self.wordAttention = nn.Sequential(
-            nn.Linear(wordLevelVariables['hiddenSize']*2, 1),
-            nn.Tanh()
+        self.wordLevel = wordLevel
+        self.sentenceLevel = sentenceLevel
+
+        self.documentClassifcation = nn.Sequential(
+            nn.Linear(2 * sentenceLevel.hiddenSize, 45),
+            nn.ReLU(),
+            nn.Linear(45, 45),
+            nn.ReLU(),
+            nn.Linear(45, 30),
+            nn.ReLU(),
+            nn.Linear(30, numCategories),
+            nn.Softmax()
         )
 
-        self.sentenceEncoder = nn.GRU(sentenceLevelVariables['inputSize'], sentenceLevelVariables['hiddenSize'], sentenceLevelVariables['numLayers'], batch_first=True, bidirectional=True)
-        self.sentenceAttention = nn.Sequential(
-            nn.Linear(sentenceLevelVariables['hiddenSize']*2, 1),
-            nn.Tanh()
-        )
+
+    def forward(self, inputText):
+        #Change this later
+        embedding = nn.Embedding(vocab_size = 0, embedding_dim=0)
+        
+
+
 
 
 
