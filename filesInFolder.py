af79f4cf5035e34233924c354c9d652d09d84d6a
Hui137 <junxionghui.137@gmail.com>
2025-02-22 20:09:16 -0500

    imporved filesInFolder

--- filesInFolder.py
+++ filesInFolder.py
@@ -1,12 +1,40 @@
 from pathlib import Path
+import docx2txt
 
-def filesInFolder(folder):
-    folder = Path(folder)
-    return [str(file.resolve()) for file in folder.rglob("*.pdf")]
+class Extractor:
+    def __init__(self, path):
+        self.path = path
+
+    def showPath(self):
+        print(f"Path: {self.path}")
+
+    def pdfsInFolder(self):
+        folder = Path(self.path)
+        return [str(file.resolve()) for file in folder.rglob("*.pdf")]
+    
+    def docxsInFolder(self):
+        folder = Path(self.path)
+        return [str(file.resolve()) for file in folder.rglob("*.docx")]
+    
+    def docxscontents(self):
+        pathes = self.docxsInFolder()
+        result = []
+        for i in range(len(pathes)):
+            content = docx2txt.process(pathes[i])
+            result.append([pathes[i], content])
+
+        return result
+
+
+
 
 
 if __name__ == "__main__":
     target_folder = input("Enter the folder: ").strip()
-    
-    print("Result(s): ")
-    print(*filesInFolder(target_folder), sep="\n")
+    extractor1 = Extractor(target_folder)
+    print("PDF:")
+    print(extractor1.pdfsInFolder())
+    print("\nDOCX:")
+    print(extractor1.docxsInFolder())
+    print("\nDOCX_content:")
+    print(extractor1.docxscontents())
