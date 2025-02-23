92310646a19ad0ad5ff93104ca0aed1b892dd1c6
CoffeCup12 <shaozhu19@gmail.com>
2025-02-23 10:03:34 -0500

    added comments

--- model.py
+++ model.py
@@ -2,38 +2,52 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F 
 import re
-    
+#This definces the word level and sentence level nerual netword of an HAN 
+#This part of the network includes an encoder(GRU) and an attension layer     
 class ProcessingNetwork(nn.Module):
     def __init__(self, inputSize, hiddenSize, numLayers):
         super(ProcessingNetwork, self).__init__()
 
+        #instance vairable 
         self.hiddenSize = hiddenSize
         self.numLayers = numLayers
 
+        #encoder 
         self.gru = nn.GRU(inputSize, hiddenSize, numLayers, batch_first=True, bidirectional=True)
+
+        #attension layer 
         self.attention = nn.Sequential(
             nn.Linear(hiddenSize*2, 1),
             nn.Tanh()
         )
     
     def forward(self, x):
+        #forward encoder pass 
         output, _ = self.gru(x)
+        #attension to get importance 
         attentionWeights = F.softmax(self.attention(output), dim=1)
+        #output
         contextVector = torch.sum(attentionWeights * output, dim=1)
         return contextVector
 
+#This class models the HAN modoled based on a word level network, a sentence level network and a final
+#evaluation network 
 class HANModel(nn.Module):
     
     def __init__(self, wordHiddenSize, sentenceHiddenSize, numLayers, vocab, embeddingDim, numCategories):
         super(HANModel, self).__init__()
+        #instance variabels, embedder and vocab dictionary 
         self.embeddingDim = embeddingDim
         self.vocab = vocab
 
+        #word and sentence level new work 
         self.wordLevel = ProcessingNetwork(embeddingDim, wordHiddenSize, numLayers)
         self.sentenceLevel = ProcessingNetwork(2 * wordHiddenSize, sentenceHiddenSize, numLayers)
 
+        #embedding level for the work level network that translate word to vectors 
         self.embedding = nn.Embedding(len(vocab), embeddingDim)
 
+        #final evaluation network 
         self.documentClassifcation = nn.Sequential(
             nn.Linear(2 * self.sentenceLevel.hiddenSize, numCategories),
             nn.Softmax(dim=1)
@@ -41,31 +55,38 @@ class HANModel(nn.Module):
 
 
     def forward(self, inputDocument):
-
+        
+        #separate document into a list of sentencs 
         sentences = self.separateSentences(inputDocument)
         processedSentences = []
 
         for sentence in sentences:
-            
+            #map each word in the sentence to a unique integer representation 
             wordsToIndex = self.separateWords(sentence)
+            #for edge cases, if sentence is empty make it <PAD>
             if wordsToIndex == []:
                 wordsToIndex = [0]
+            #embedding 
             embeds = self.embedding(torch.LongTensor(wordsToIndex)).unsqueeze(0)
-
+            #process the word rep vector to the word level network to get the processed sentence 
             processedSentence = self.wordLevel.forward(embeds)
             processedSentences.append(processedSentence)
 
+        #pass the sentences to the sentence level network 
         sentenceTensor = torch.cat(processedSentences, dim=0).unsqueeze(0)
         documentVector = self.sentenceLevel.forward(sentenceTensor)
 
+        #pass the document vector the classifcation layer
         return self.documentClassifcation(documentVector)
 
         
     def separateSentences(self, document):
+        #separate document into sentences 
         sentenceSeparators = "(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|!)\s"
         return re.split(sentenceSeparators, document)
     
     def separateWords(self, sentence):
+        #get int rep of words in a sentence 
         words = sentence.lower().split()
         return [self.vocab.get(word, self.vocab['<UNK>']) for word in words]  # Handle unknown words
     
--- training.py
+++ training.py
@@ -7,85 +7,100 @@ import fitz
 import docx2txt
 import random
 
+#the class models the trainning loop of the HAN
 class trainer():
 
     def __init__(self, path):
+        #path to the folder that contains subfolders with organized files in each subfolder 
         self.path = path
+        #instance varibles 
         self.vocab = {"<PAD>": 0, "<UNK>": 1}
         self.punctuation = ['.', ',', '!', '?', ':', ';', '(', ')', '[', ']', '{', '}', '<', '>', '"', "'"]
 
     def extractTextFromPdf(self, path):
+        #read text from a pdf 
         doc = fitz.open(path)
         text = ""
         for page in doc:
+            #get rid of repeating space and .s
             text += re.sub(r"([. ])\1+", r"\1", page.get_text()).strip()
         return text
 
     def generateVocab(self, text):
+        #split the text of the entire pdf to words 
         words = text.lower().split()
+        #create a mapping:str -> int to that word 
         for word in words:
             if word not in self.vocab and word != '' and len(word) < 10 and word not in self.punctuation:
                 self.vocab.update({word: len(self.vocab)})
     
-    # Function to traverse the folder and process all PDFs
     def processFilesInDirectory(self):
+        #create list of text, label pairs 
         pdfData = []
         label = 0
 
-        print(self.path)
+        #looks in every subfolder of the folder 
         listOfFolders = os.listdir(self.path)
         for folders in listOfFolders:
 
             folderPath = self.path + "/" + folders
             listOfFiles = os.listdir(folderPath)
-
+            #looks in every file in each subfolder 
             for file in listOfFiles:
                 filePath = folderPath + "/" + file
                 text = ''
-
+                #generate text
                 if file.endswith(".pdf"):
                     text = self.extractTextFromPdf(filePath)
                 elif file.endswith(".docx"):
                     text = docx2txt.process(filePath)
-                
+                #append text, label pair 
                 if text != '':
                     pdfData.append((text, label))
                     self.generateVocab(pdfData[-1][0])   
+            #update lable 
             label += 1
 
         return pdfData, label
     
     def train(self):
-        documents, numCatgory = self.processFilesInDirectory()
+        #get the text label pair and the number of category to sotry 
+        documents, numCategory = self.processFilesInDirectory()
 
+        #shuffle the list
         random.shuffle(documents)
 
+        #separate train and tes dataset 
         train = documents[0:int(0.8*len(documents))]
         test = documents[int(0.8*len(documents)):]
 
-        HAN = model.HANModel(wordHiddenSize=64, sentenceHiddenSize=128, numLayers=2, embeddingDim=300, vocab=self.vocab, numCategories= numCatgory)
+        #create model 
+        HAN = model.HANModel(wordHiddenSize=64, sentenceHiddenSize=128, numLayers=2, embeddingDim=300, vocab=self.vocab, numCategories= numCategory)
 
+        #loss function and optimizer 
         criterion = torch.nn.CrossEntropyLoss()
         optimizer = optim.Adam(HAN.parameters(), lr=0.001)
-
+        
+        #trainnning loop 
         epochs = 10
         for i in range(epochs):
-            total_loss = 0  # Initialize total loss for the epoch
+            # Initialize total loss for the epoch
+            total_loss = 0  
             for text, label in train:
                 
                 optimizer.zero_grad()
                 label = torch.tensor([label])
-
+                #caclulate loss 
                 output = HAN.forward(text)
                 loss = criterion(output, label)
                 
-                # Add the loss of this batch to the total loss
+                #add the loss of this batch to the total loss
                 total_loss += loss.item()
-
+                #backpropgation 
                 loss.backward()
                 optimizer.step()
             
-            # Print the average loss for this epoch
+            #print the average loss for this epoch
             avg_loss = total_loss / len(documents)
             #print(f"Epoch {i+1}/{epochs}, Average Loss: {avg_loss:.4f}")
 
@@ -98,10 +113,11 @@ class trainer():
 
 
 if __name__ == "__main__":
-
+    #for testing purpose 
     trainer = trainer(input("Enter testing Path"))
     test, HAN = trainer.train()
     
+    #output accurary from testing data set 
     numCorrect = 0
     for doc, label in test:
         output = HAN.forward(doc)
