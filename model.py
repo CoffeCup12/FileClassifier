added masking and some bug fix

model.py
@@ -31,21 +31,21 @@ class Processing_network(nn.Module):
         )
         self.context_vector = nn.Parameter(torch.randn(1, 2*hidden_size), requires_grad=True)
     
-    def forward(self, x):
+    def forward(self, x, mask):
         """returns a vector of dim (1,hidden_size) that represents the word/sentence"""
-        x,_ = self.encoder(x)
-        
-        if(isinstance(x, PackedSequence)):
-            x,_ = pad_packed_sequence(x, batch_first = True)
 
-        h = x.clone().detach()
-        x = self.mlp(x)
+        x,_ = self.encoder(x)
+        h,_ = pad_packed_sequence(x, batch_first = True)
+
+        x = self.mlp(h)
         
         #softmax with context vector
         x = x * self.context_vector.repeat(x.size()[0], x.size()[1], 1)
-        
         x = torch.sum(x, dim=2, keepdim = True)
+  
+        x = x.masked_fill(mask==0, -1e9)
         x = F.softmax(x, dim=1)
+        
         #weighted sum
         x = x.repeat(1,1,h.size()[2]) * h
         x = torch.sum(x, dim=1)
@@ -60,22 +60,30 @@ class Doc_network(nn.Module):
         self.sentence_network = Processing_network(100,50)
         self.classifcation = nn.Sequential(
             nn.Linear(100,num_classes),
-            nn.Softmax(dim=0)
+            nn.LogSoftmax(dim=1)
         )
         self.tokenizer = spacy.load("en_core_web_md")
 
+
     def forward(self,x):
-        """retrun a (batch_size,num_classes) vector that represents the probability of the document's belonging """
+        """retrun a (batch_size,num_classes) matrix that represents the probability of the document's belonging """
         batched_input = [sent_tokenize(doc) for doc in x]
         
-        batched_sentences_rep = [self.process_sentence_rep(sentences) for sentences in batched_input]
-        length = [rep.size(0) for rep in batched_sentences_rep]
-
+        batched_sentences_rep = []
+        length = []
+        mask = []
+        for sentences in batched_input:
+            sentences_rep = self.process_sentence_rep(sentences)
+            batched_sentences_rep.append(sentences_rep)
+            length.append(sentences_rep.size(0))
+            mask.append(torch.ones(sentences_rep.size()[0], 1))
+            
         #add paddig
         padded_sentences = pad_sequence(batched_sentences_rep, batch_first=True)
+        mask = pad_sequence(mask, batch_first=True)
 
         packed_input = pack_padded_sequence(padded_sentences, length, batch_first=True, enforce_sorted=False)
-        batched_doc_rep = self.sentence_network(packed_input)
+        batched_doc_rep = self.sentence_network(packed_input, mask)
 
         #classification
         final = self.classifcation(batched_doc_rep)
@@ -83,22 +91,39 @@ class Doc_network(nn.Module):
         return final
     
     def process_sentence_rep(self, sentences):
-        """returns a (sequence length, 100) vector representations for all sentences in a document """
-        sentences_rep = []
+        """return a (length of sequence, 100) matrix with each row represents a sentence """
+        batched_embedding = []
+        length = []
+        mask = []
         for sentence in sentences:
             tokens = self.tokenizer(sentence)
             embeddings = torch.tensor(np.array([token.vector for token in tokens]))
-            #generate (1, 100) vectors for each sentence 
-            output = self.word_network(embeddings.unsqueeze(0))
-            sentences_rep.append(output.squeeze(0))
 
-        #print(torch.stack(sentences_rep).size())
-        return torch.stack(sentences_rep)
+            batched_embedding.append(embeddings)
+            length.append(embeddings.size()[0])
+            mask.append(torch.ones(embeddings.size()[0],1))
+        
+        mask = pad_sequence(mask, batch_first=True)
+        padded_input = pad_sequence(batched_embedding, batch_first=True)
+
+        packed_input = pack_padded_sequence(padded_input, length, batch_first = True, enforce_sorted=False)
+
+        return self.word_network(packed_input, mask)
+        
 
 
 if __name__ == "__main__":
     model = Doc_network(2)
-    text = ("unclassified sentence1. Sentence2. This is a sample sentence.", "hello world.", "the fox jumps over the lazy dog")
-    print(model(text))
+    inputs = ("unclassified sentence1. Sentence2. This is a sample sentence.", "hello world.", "the fox jumps over the lazy dog")
+    #model = Processing_network(3,5)
+
+    #inputs = [torch.randn(1,2,3), torch.randn(1,4,3)]
+
+    #length = [inputs[0].size()[0],inputs[1].size()[0]]
+
+    #padded_input = pad_sequence(inputs, batch_first=True)
+    #packed_input = pack_padded_sequence(padded_input,length,batch_first=True,enforce_sorted=False)
+
+    print(model(inputs))
     
 

training.py
@@ -19,6 +19,8 @@ class File_Dataset(Dataset):
         text = self.reader.read(meta_data["path"])
         if text == "unclassified":
             label = len(self.subfolders)
+            with open("dummy.txt", 'r') as file:
+                text = file.read()
         else:
             label = meta_data["label"]
         return text, label
@@ -36,19 +38,22 @@ class File_Dataset(Dataset):
 
 
 class Trainer():
-    def __init__(self, root_dir):
+    def __init__(self, root_dir, batch_size):
         dataset = File_Dataset(root_dir)
-        self.loader = DataLoader(dataset, batch_size=32, shuffle = True)
+        self.batch_size = batch_size
+        self.loader = DataLoader(dataset, batch_size=self.batch_size, shuffle = True)
         model = Doc_network(len(os.listdir(root_dir))+1)
         self.model = model.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
 
     def train(self, epoch):
         loss_fn = torch.nn.NLLLoss()
-        optimizer = torch.optim.SGD(self.model.parameters(), lr=0.001, momentum = 0.9)
-
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
+        
         for i in range(epoch):
             running_loss = 0.0
-            count = 0
+            total = 0
+            correct = 0
+
             for text, label in self.loader:
                 optimizer.zero_grad()
 
@@ -59,10 +64,18 @@ class Trainer():
                 
                 optimizer.step()
 
-                running_loss += loss.item()
-                count+=1
+                running_loss += loss.item() * self.batch_size
+                total += self.batch_size
+                
+                with torch.no_grad():
+                    preds = torch.argmax(torch.exp(output), dim=1)
+                    correct += (preds == label).sum().item()
 
-            print(running_loss/count)
+            
+            avg_loss = running_loss / total
+            accuracy = correct / total * 100
+
+            print(f"Epoch {i}, avg_loss: {avg_loss}, accuracy: {accuracy}")
 
         torch.save(self.model.state_dict(), "model.pth")
     